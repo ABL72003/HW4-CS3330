5354a130e9cdbcdf218edb2b7de78615f28a3a0c
ABL72003 <149620761+ABL72003@users.noreply.github.com>
2025-04-23 23:24:39 -0500

    Update Simulation.java

--- src/hw4/Simulation.java
+++ src/hw4/Simulation.java
@@ -5,15 +5,16 @@ import hw4.game.Game;
 public class Simulation {
 	
     public static void main(String[] args) {
-    	 // Create a new game
-        Game game = new Game(5);
+    	 //Create a new game
+	int randomSize = 3 + (int)(Math.random() * 5); // Random int from 3 to 7
+        Game game = new Game(randomSize);
         
-        // Print the initial game state
+        //Print the initial game state
         System.out.println("Initial game state:");
         System.out.println(game.getGrid()); 
         game.getGrid().printGrid();
         
-        // Start the game
+        //Start the game
         System.out.println("Game started...");
         
 	}
