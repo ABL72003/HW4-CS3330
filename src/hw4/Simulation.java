e5174a1889aad49b4ce64e60b1cc739724e2f2c7
Alexis <alexispaten@gmail.com>
2025-04-23 23:35:06 -0500

    Added more code to simulation

--- src/hw4/Simulation.java
+++ src/hw4/Simulation.java
@@ -1,12 +1,20 @@
 package hw4;
 
 import hw4.game.Game;
+import hw4.maze.Grid;
+
 
 public class Simulation {
 	
     public static void main(String[] args) {
-    	 // Create a new game
-        Game game = new Game(5);
+    	 
+    	// Create a new game
+    	Grid grid = new Grid();
+        Game game = new Game(grid);
+        
+        //creates game 2
+        Grid grid2 = new Grid();
+        Game game2 = new Game(grid2);
         
         // Print the initial game state
         System.out.println("Initial game state:");
@@ -15,6 +23,14 @@ public class Simulation {
         
         // Start the game
         System.out.println("Game started...");
+        
+        //prints the next game state
+        System.out.println("Initial game state:");
+        System.out.println(game2.getGrid()); 
+        game2.getGrid().printGrid();
+        
+        // Start the game
+        System.out.println("Game 2 started...");
         
 	}
 
